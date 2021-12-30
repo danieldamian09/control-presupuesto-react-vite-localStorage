3f404dcce18eb6c8b3235f60e9f2517f4388b70b
danieldamian09 <humbertorivero9@gmail.com>
2021-12-30 19:37:25 -0300

    Validando el Presupuesto

--- control-gastos/src/components/NuevoPresupuesto.jsx
+++ control-gastos/src/components/NuevoPresupuesto.jsx
@@ -1,7 +1,27 @@
+import React, { useState } from 'react';
+import Mensaje from './Mensaje';
+
+
 const NuevoPresupuesto = ( {presupuesto, setPresupuesto} ) => {
+
+    // Estado para el mensaje cuando el presupuesto no es valido o es menor a cero
+    const [mensaje, setMensaje] = useState("")
+
+
+    // Validar campo del preupuesto que no sea un string o monto menor a cero
+    const handlePresupuesto = (e) => {
+        e.preventDefault();
+        if(!Number(presupuesto) || Number(presupuesto) < 0){
+            setMensaje("No es un Presupuesto Valido")
+        }else{
+          console.log("El presupuesto es Valido")
+        }
+    }
+
+
   return (
     <div className="contenedor-presopuesto contendor sombra">
-      <form className="formulario">
+      <form onSubmit={handlePresupuesto} className="formulario">
         <div className="campo">
           <label>Definir Presupuesto</label>
           <input
@@ -14,6 +34,8 @@ const NuevoPresupuesto = ( {presupuesto, setPresupuesto} ) => {
           />
         </div>
         <input type="submit" value="Añadir" />
+        {/* Mensaje en caso de que el monto no pase la validacion */}
+        {mensaje && <Mensaje tipo="error">{mensaje}</Mensaje>}
       </form>
     </div>
   );
