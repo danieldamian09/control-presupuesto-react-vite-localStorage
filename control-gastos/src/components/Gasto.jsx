52eecf799a42783ef3f85004276e9eeeace6ff13
danieldamian09 <humbertorivero9@gmail.com>
2022-01-10 12:37:48 -0300

    formatear las fechas

--- control-gastos/src/components/Gasto.jsx
+++ control-gastos/src/components/Gasto.jsx
@@ -1,19 +1,22 @@
+import {formatearFecha } from '../helpers'
 
 const Gasto = ({gasto}) => {
 
   // Destructuring para sacar las variables que necesitamos del objeto gasto
-  const {nombre, cantidad, categoria, id} = gasto;
+  const {nombre, cantidad, categoria, id, fecha} = gasto;
   return (
     <div className="gasto sombra">
       <div className="contenido-gasto">
-
-
-
         <div className="descripcion-gasto">
             <p className="categoria">{categoria}</p>
             <p className="nombre-gasto">{nombre}</p>
+            <p className="fecha-gasto">
+              Agregado el: {''}
+              <span>{formatearFecha(fecha)}</span>
+            </p>
         </div>
       </div>
+      <p className='cantidad-gasto'>${cantidad}</p>
     </div>
   )
 }
