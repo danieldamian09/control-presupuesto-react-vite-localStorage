95ebb823eb16dd889f69c78e0278c69ce6b77476
danieldamian09 <humbertorivero9@gmail.com>
2022-01-10 16:12:59 -0300

    mostrando los iconos de gastos

--- control-gastos/src/App.jsx
+++ control-gastos/src/App.jsx
@@ -50,7 +50,7 @@ function App() {
   }
 
   return (
-    <div>
+    <div className={modal && "fijar"}>
       <Header
         presupuesto={presupuesto}
         setPresupuesto={setPresupuesto}
--- control-gastos/src/components/Gasto.jsx
+++ control-gastos/src/components/Gasto.jsx
@@ -1,24 +1,47 @@
-import {formatearFecha } from '../helpers'
+import { formatearFecha } from "../helpers";
+// Iconos Gastos
+import IconoAhorro from "../img/icono_ahorro.svg";
+import IconoComida from "../img/icono_comida.svg";
+import IconoGastos from "../img/icono_gastos.svg";
+import IconoCasa from "../img/icono_comida.svg";
+import IconoOcio from "../img/icono_ocio.svg";
+import IconoSalud from "../img/icono_salud.svg";
+import IconoSuscripciones from "../img/icono_suscripciones.svg";
 
-const Gasto = ({gasto}) => {
+// Creamos un diccionario de Iconos
+const diccionarioIconos = {
+  ahorro: IconoAhorro,
+  comida: IconoComida,
+  casa: IconoCasa,
+  gastos: IconoGastos,
+  ocio: IconoOcio,
+  salud: IconoSalud,
+  suscripciones: IconoSuscripciones
+};
 
+const Gasto = ({ gasto }) => {
   // Destructuring para sacar las variables que necesitamos del objeto gasto
-  const {nombre, cantidad, categoria, id, fecha} = gasto;
+  const { nombre, cantidad, categoria, id, fecha } = gasto;
   return (
     <div className="gasto sombra">
       <div className="contenido-gasto">
+        {/* Imagenes */}
+        <img 
+            src={diccionarioIconos[categoria]} 
+            alt="iconos-gastos" 
+        />
         <div className="descripcion-gasto">
-            <p className="categoria">{categoria}</p>
-            <p className="nombre-gasto">{nombre}</p>
-            <p className="fecha-gasto">
-              Agregado el: {''}
-              <span>{formatearFecha(fecha)}</span>
-            </p>
+          <p className="categoria">{categoria}</p>
+          <p className="nombre-gasto">{nombre}</p>
+          <p className="fecha-gasto">
+            Agregado el: {""}
+            <span>{formatearFecha(fecha)}</span>
+          </p>
         </div>
       </div>
-      <p className='cantidad-gasto'>${cantidad}</p>
+      <p className="cantidad-gasto">${cantidad}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Gasto
+export default Gasto;
